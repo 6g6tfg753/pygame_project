7d471e896434b57368b0dc1154a2f02c65d90d08
6g6tfg753 <dmitreevmaxsim1999@gmail.com>
2025-01-17 13:33:40 +0300

    Update maps.py
    boxes

--- maps.py
+++ maps.py
@@ -1,5 +1,5 @@
 import pygame
-# import fil
+
 
 class Maps:
     def map_init(self, w, h=0):
@@ -12,7 +12,7 @@ class Maps:
     def field_init(self, level):
         level = int(level)
         map_9 = []
-        # 1 - cage; 0 - empty cell; -1 - finish; 2 -weapon; 3 - enemy
+        # 1 - cage; 0 - empty cell; -1 - finish; 2 -weapon; 3 - enemy, 4 - move_box, 5 - button
         if level == 1:
             file = open('maps/levels/level1.txt', 'r').readlines()
         elif level == 2:
@@ -64,4 +64,23 @@ class Maps:
                     sprite1.rect.x = self.Size_im * a
                     sprite1.rect.y = self.Size_im * b
                     self.enemies.add(sprite1)
+                    self.all_sprites.add(sprite1)
+                if map_9[b][a] == 4:
+                    sprite_box = pygame.sprite.Sprite(self.all_sprites)
+                    sprite_box.image = pygame.image.load('maps/box_move.png')
+                    sprite1.image.set_colorkey((221, 221, 221))
+                    sprite_box.rect = sprite_box.image.get_rect()
+                    sprite_box.rect.x = a * 100
+                    sprite_box.rect.y = b * 100
+                    self.move_boxes.add(sprite_box)
+                    self.sprite_box = sprite_box
+
+
+                if map_9[b][a] == 5:
+                    sprite1 = pygame.sprite.Sprite(self.all_sprites)
+                    sprite1.image = pygame.image.load('maps/button.png')
+                    sprite1.rect = sprite1.image.get_rect()
+                    sprite1.rect.x = self.Size_im * a
+                    sprite1.rect.y = self.Size_im * b
+                    self.buttons.add(sprite1)
                     self.all_sprites.add(sprite1)
